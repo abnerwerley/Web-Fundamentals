462812ae7913c04356d5e5bfc4e8669c5f3f2d27
abnerwerley <abnerwerley77@gmail.com>
2021-10-22 15:47:10 -0300

    [Feature Abner - Task: Initializing Js]

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// how to declare a variable:
+var name = "abner"; //ancient way to declare a variable
+let name2 = "Abner"; //if you want to declare a changeble variable
+const name3 = "Abner Werley"; // a "fix" variable
+
+// alert(name);
+// alert(name2);
+// alert(name3);
